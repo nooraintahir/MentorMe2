66a1ae48140cf7ea7e6a36a10f0487901954b7d4
Noorain Tahir <noorain.tahirr@gmail.com>
2024-02-10 00:54:40 +0500

    screen3

--- app/src/main/java/com/nooraintahir/i200517/SignupActivity.kt
+++ app/src/main/java/com/nooraintahir/i200517/SignupActivity.kt
@@ -11,8 +11,8 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SignupActivity : AppCompatActivity() {
 
-    private val countries = arrayOf("Select Country", "USA", "Canada", "UK", "Australia")
-    private val cities = arrayOf("", "New York", "Los Angeles", "Chicago", "Houston")
+    private val countries = arrayOf("Select Country", "Pakistan", "Malaysia", "Singapore", "Taiwan")
+    private var cities = arrayOf("Select City")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,26 +23,42 @@ class SignupActivity : AppCompatActivity() {
         val countrySpinner = findViewById<Spinner>(R.id.country)
         countrySpinner.adapter = countryAdapter
 
-        val cityAdapter = ArrayAdapter(this, R.layout.spinner_design, cities)
-        cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         val citySpinner = findViewById<Spinner>(R.id.city)
-        citySpinner.adapter = cityAdapter
-
+        //adding hint for country spinner
         countrySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                val value = parent!!.getItemAtPosition(position).toString()
-                if(value == countries[0]){
-                    (view as TextView).setTextColor(Color.GRAY)
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedCountry = countries[position]
+                cities = when (selectedCountry) {
+                    "Pakistan" -> arrayOf("Select City", "Rawalpindi", "Islamabad", "Lahore", "Karachi")
+                    "Malaysia" -> arrayOf("Select City", "Kuala Lumpur", "Penang", "Johor Bahru", "Ipoh")
+                    "Singapore" -> arrayOf("Select City", "Singapore City", "Jurong West", "Bedok", "Tampines")
+                    "Taiwan" -> arrayOf("Select City", "Taipei", "Kaohsiung", "Taichung", "Tainan")
+                    else -> arrayOf("Select City")
+                }
+                val cityAdapter = ArrayAdapter(this@SignupActivity, R.layout.spinner_design, cities)
+                cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                citySpinner.adapter = cityAdapter
+                if(selectedCountry == countries[0]){
+                    (view as TextView).setTextColor(Color.parseColor("#A9A9A9"))
                 }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Do something
+                // Do Something
+            }
+        }
+
+        //adding hint for city spinner
+        citySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val value = parent!!.getItemAtPosition(position).toString()
+                if(value == cities[0]){
+                    (view as TextView).setTextColor(Color.parseColor("#A9A9A9"))
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do Something
             }
         }
 
