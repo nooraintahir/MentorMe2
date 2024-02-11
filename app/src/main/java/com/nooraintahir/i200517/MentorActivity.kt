912278576e79ab3c33777cc0bdc97afcf5d5e9fd
Noorain Tahir <noorain.tahirr@gmail.com>
2024-02-12 01:20:12 +0500

    screen11

--- app/src/main/java/com/nooraintahir/i200517/MentorActivity.kt
+++ app/src/main/java/com/nooraintahir/i200517/MentorActivity.kt
@@ -1,11 +1,19 @@
 package com.nooraintahir.i200517
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MentorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mentor)
+
+        val reviewbtn = findViewById<Button>(R.id.review)
+        reviewbtn.setOnClickListener {
+            val intent = Intent(this, MentorReviewActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
