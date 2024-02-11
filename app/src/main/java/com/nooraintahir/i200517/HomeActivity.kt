078d75c0230e29e1515e1c5979db621bbc3e2ff8
Noorain Tahir <noorain.tahirr@gmail.com>
2024-02-12 00:39:19 +0500

    screen10

--- app/src/main/java/com/nooraintahir/i200517/HomeActivity.kt
+++ app/src/main/java/com/nooraintahir/i200517/HomeActivity.kt
@@ -1,10 +1,12 @@
 package com.nooraintahir.i200517
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class HomeActivity : Fragment() {
     override fun onCreateView(
@@ -12,7 +14,15 @@ class HomeActivity : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_home, container, false)
+        val view = inflater.inflate(R.layout.activity_home, container, false)
+
+        val mentorbtn = view.findViewById<Button>(R.id.mentorpage)
+
+        mentorbtn.setOnClickListener {
+            val intent = Intent(requireContext(), MentorActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 }
--- app/src/main/java/com/nooraintahir/i200517/SearchActivity.kt
+++ app/src/main/java/com/nooraintahir/i200517/SearchActivity.kt
@@ -1,6 +1,5 @@
 package com.nooraintahir.i200517
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
