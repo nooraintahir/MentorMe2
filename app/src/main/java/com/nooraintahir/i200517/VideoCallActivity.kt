41211f5abf36e79e5ea928c76b49c7f431ad4e16
Noorain Tahir <noorain.tahirr@gmail.com>
2024-02-13 00:08:18 +0500

    screen19

---
+++ app/src/main/java/com/nooraintahir/i200517/VideoCallActivity.kt
@@ -0,0 +1,11 @@
+package com.nooraintahir.i200517
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VideoCallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_call)
+    }
+}
